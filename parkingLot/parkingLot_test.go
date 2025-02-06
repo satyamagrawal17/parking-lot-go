6bbb8595841acb4ad9188984cade9734f9acda55
satyamagrawal17 <satyam.agrawal_ftc@external.swiggy.in>
2025-02-06 16:28:35 +0530

    Tested parking lot creation for negative and zero slots given

--- parkingLot/parkingLot_test.go
+++ parkingLot/parkingLot_test.go
@@ -8,3 +8,17 @@ func TestParkingLotReturnNoExceptionWhenCreateParkingLotWithFiveSlots(t *testing
 		t.Errorf("Expected parkingLot instance, got nil")
 	}
 }
+
+func TestParkingLotReturnExceptionWhenCreateParkingLotWithZeroSlots(t *testing.T) {
+	_, err := NewParkingLot(0)
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+}
+
+func TestParkingLotReturnExceptionWhenCreateParkingLotWithMinus5Slots(t *testing.T) {
+	_, err := NewParkingLot(-5)
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+}
