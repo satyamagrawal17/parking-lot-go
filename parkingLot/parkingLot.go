a74c4f2337816cc9226412184e59d06dea9325e8
satyamagrawal17 <satyam.agrawal_ftc@external.swiggy.in>
2025-02-06 16:24:31 +0530

    Tested parking lot creation

--- parkingLot/parkingLot.go
+++ parkingLot/parkingLot.go
@@ -3,19 +3,20 @@ package parkingLot
 import (
 	"errors"
 	"parking_lot/slot"
+	"parking_lot/ticket"
 )
 
 type ParkingLot struct {
-	slots              []slot.Slot
-	ticketToSlotMapper map[Ticket]slot.Slot
+	slots              []*slot.Slot
+	ticketToSlotMapper map[*ticket.Ticket]*slot.Slot
 }
 
 func NewParkingLot(capacity int) (*ParkingLot, error) {
 	if capacity <= 0 {
 		return nil, errors.New("Invalid capacity")
 	}
-	slots := make([]slot.Slot, capacity)
-	ticketToSlotMapper := make(map[Ticket]slot.Slot)
+	slots := make([]*slot.Slot, capacity)
+	ticketToSlotMapper := make(map[*ticket.Ticket]*slot.Slot)
 	return &ParkingLot{
 		slots:              slots,
 		ticketToSlotMapper: ticketToSlotMapper,
